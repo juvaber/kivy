0c41fa9af5a726c4cd307f1ef16618ac405a6f74
juvaber <juan.valero@gmail.com>
2016-08-11 22:38:20 +0200

    Añado archivo .gitignore

--- aprendekivy/aprendekivy01/main.py
+++ aprendekivy/aprendekivy01/main.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 
 class MyApp(App):
    def build(self):
-      return Label(text='Hola mundo!!')
+      return Label(text='Hola mundo cruel!!')
 
 if __name__ == '__main__':
    MyApp().run()
