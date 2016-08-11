339846d834d7533ddeeb4d45b60dcbabb86ec0af
juvaber <juan.valero@gmail.com>
2016-08-11 22:48:48 +0200

    mas cambios....

--- aprendekivy/aprendekivy01/main.py
+++ aprendekivy/aprendekivy01/main.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 
 class MyApp(App):
    def build(self):
-      return Label(text='Hola mundo cruel!!')
+      return Label(text='Hola mundo!')
 
 if __name__ == '__main__':
    MyApp().run()
